a5eb7a45b0d3297ffe71730bef679ecb6695a53f
gustavo suero <tsuero.s@gmail.com>
2016-03-18 12:52:16 -0500

    Controls Init
    Controles para el murp

---
+++ flikrcomponent/BadrHomeView.cs
@@ -0,0 +1,30 @@
+﻿using System; 
+using System.Drawing;
+using UIKit;
+using CoreGraphics;
+using System.Collections.Generic;
+using Foundation;
+
+namespace Badr
+{
+	public class BadrHomeView : UIView
+	{
+		public BadrHomeView (nfloat x, nfloat y,nfloat width, nfloat height)
+		{
+			AutoresizingMask = UIViewAutoresizing.FlexibleDimensions;
+
+			_width = width;
+			_height = height;
+
+			Frame = new CGRect (x, y, width, height);
+
+
+		}
+
+
+		//settings
+		nfloat _width = 1024 ;
+		nfloat _height =  768 ;
+	}
+}
+
---
+++ flikrcomponent/BadrScrollView.cs
@@ -0,0 +1,36 @@
+﻿using System; 
+using System.Drawing;
+using UIKit;
+using CoreGraphics;
+using System.Collections.Generic;
+using Foundation;
+
+namespace Badr
+{
+	public class BadrScrollView : UIView
+	{
+		public BadrScrollView (nfloat x, nfloat y,nfloat width, nfloat height)
+		{
+			AutoresizingMask = UIViewAutoresizing.FlexibleDimensions;
+
+			_width = width;
+			_height = height;
+			_x = x;
+			_y = y;
+
+			Frame = new CGRect (x, y, width, height);
+
+		}
+
+		//settings
+		nfloat _width = 1024 ;
+		nfloat _height = 768 ;
+		nfloat _x = 0 ;
+		nfloat _y = 0 ;
+
+
+
+
+	}
+}
+
